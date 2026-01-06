96e47011f8fa69a58872dc92f7b8e733527f5ff2
Tan155 <Jumbotan1510@gmail.com>
2026-01-06 23:10:07 +0700

    Add files via upload

---
+++ Code_Search_Sort/Counting_Sort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int arr[] = {0,4,2,2,0,0,1,1,0,1,0,2,4,2};
+	int n = sizeof(arr) / sizeof(int);
+	
+	int count[100] = {0};
+	for (int i = 0; i < n; i++){
+		count[arr[i]]++;
+	}
+	
+	for (int i = 0; i < 100; i++){
+		while(count[i] > 0){
+			cout << i << " "; count[i]--;
+		}
+	}
+	return 0;
+}
