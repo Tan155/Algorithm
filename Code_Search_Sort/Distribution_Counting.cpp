bf6d034d479900fdaf57370ed72c4a191fd5e2b1
Tan155 <Jumbotan1510@gmail.com>
2026-01-10 19:25:55 +0700

    Refactor Distribution Counting implementation

--- Code_Search_Sort/Distribution_Counting.cpp
+++ Code_Search_Sort/Distribution_Counting.cpp
@@ -1,35 +1,37 @@
 #include<iostream>
+
+
 using namespace std;
 
 int main(){
-	int arr[] = {2,1,3,5,1,2,5,3,4,1,5};
-	int n = sizeof(arr) / sizeof(int);
-	
-	int max=-100, min=100;
+	int A[] = {16, 25, 2, 54, 36, 9, 12, 66};
+	int n = sizeof(A) / sizeof(A[0]);
+	int max=0,min=100;
 	for (int i = 0; i < n; i++){
-		if(arr[i] > max) max = arr[i];
-		if(arr[i] < min) min = arr[i];
+		if(A[i] > max) max = A[i];
+		if(A[i] < min) min = A[i];
 	}
 	
 	int range = max - min + 1;
-	int count[range] = {0};
-	int output[n];
+	int D[range] = {0};
+	int S[n];
 	
 	for (int i = 0; i < n; i++){
-		count[arr[i] - min]++;
+		D[A[i] - min] = D[A[i] - min] + 1;
 	}
 	
 	for (int i = 1; i < range; i++){
-		count[i] += count[i - 1];
+		D[i] = D[i - 1] + D[i];
 	}
 	
-	for (int i = n - 1; i >= 0; i--){
-		output[count[arr[i] - min] - 1] = arr[i];
-		count[arr[i] - min]--;
+	for (int i = n-1; i >= 0; i--){
+		int j = A[i] - min;
+		S[D[j] - 1] = A[i];
+		D[j] = D[j] - 1;
 	}
 	
 	for (int i = 0; i < n; i++){
-		cout << output[i] << " ";
+		cout << S[i] << " ";
 	}
 	return 0;
 }
